cf3e24b213383be36fd005d99e0a520d5efaea5f
jennie9 <jenniferokolo9@gmail.com>
2022-10-14 09:26:38 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: Always to 0
+ */
+
+int main(void)
+{
+	print("\"programming is like building a multi lingual puzzle");
+	return (0);
+
+}
