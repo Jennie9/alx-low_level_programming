1884f449e10740e7736d83e7ffd6bddf11fa2c9d
jennie9 <jenniferokolo9@gmail.com>
2022-10-28 13:15:43 -0700

    main

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -4,6 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: char to be printed
  * Return: 1 on success
+ * on error, -1 is returned, and error is set appropraitely
  */
 int _putchar(char c )
 {
