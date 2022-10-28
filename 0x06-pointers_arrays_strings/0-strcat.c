be96c108c00044feb8465e86628ea9797d9ae1d5
jennie9 <jenniferokolo9@gmail.com>
2022-10-28 12:09:14 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatennating two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointers to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int j;
+	int e;
+
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	e = o;
+	while (src[e] != '\0')
+	{
+		dest[j] = src[e];
+		j++;
+		e++;
+	}
+	dest[j] = '\0';
+	return (dest);
