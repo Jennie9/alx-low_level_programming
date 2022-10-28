3624b54e3fa71a380170d38e65ebaa0cefa8d224
jennie9 <jenniferokolo9@gmail.com>
2022-10-28 16:16:24 -0700

    task 44

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _reverse_array -reverse arrays of intergers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: rev
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
